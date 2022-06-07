e2876e62b49c0f6a357278ecbb01a40ae6a3ba99
Mateo <mateom.realtor@gmail.com>
2022-06-06 23:59:50 -0400

    Updated Js, Css & Html

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,10 @@
-console.log("Heyyyy")
+// Getting the Shop now button in the header section
+//Redirecting User to Mens Collection once buttton is clicked
+let shopNowBtn = document.querySelector("#shopNowBtn")
+shopNowBtn.addEventListener("click",function(){
+window.location.href = "MensCollection.html"
+    // if(window.location.href = "MensCollection.html"){
+    //     console.log(" Link WOrks ")
+    // }
+
+})
